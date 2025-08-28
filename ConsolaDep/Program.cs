5173fd99efbe9fefe6bf2b8e3f8cdc49616f2d7f
luisamtag <cielitoazul.1027@gmail>
2025-08-28 07:49:32 -0500

    Codigo

--- ConsolaDep/Program.cs
+++ ConsolaDep/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
+
+public class Deportes
+{
+    public int IdDep = 0;
+    public String? Nombre = "";
+}
+public class TiposD
+{
+    public int IdT = 0;
+    public String? Nombre = "";
+}
+
+public class EntrenadorD
+{
+    public int IdEntr = 0;
+    public String? Nombre = "";
+    public DateTime fecha = DateTime.Now;
+    public bool Activo = false;
+    public decimal Salario = 0.0m;
+    public TiposD Tipo = new TiposD();
+    public List<Deportes> Deportes = new List<Deportes>();
+}
+
+public class JugadoresD
+{
+    public int IdJug = 0;
+    public String? Nombre = "";
+    public DateTime fecha = DateTime.Now;
+    public bool Activo = false;
+    public decimal Salario = 0.0m;
+    public TiposD Tipo = new TiposD();
+    public List<Deportes> Deportes = new List<Deportes>();
+}
+
+
